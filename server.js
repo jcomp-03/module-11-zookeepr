b7e832766dd80fba594550a3be3877087383dad4
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-01 22:15:16 -0500

    Changed hardcoded port 3001 to variable PORT

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express');
 // creating a route that the front-end
 // can request data from. Start by requiring the data
 const { animals } = require('./data/animals');
+
+const PORT = process.env.PORT || 3001;
 // instantiate the server
 const app = express();
 
@@ -63,6 +65,6 @@ app.get('/api/animals', (req, res) => {
 
 
 // make our server listen by chaining the .listen method
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
