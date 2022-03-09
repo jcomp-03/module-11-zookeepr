63c587b52b7aea7b6a2dceb031da949a68e66c4c
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-03-08 22:30:02 -0500

    Built out GET and POST request routes for animals

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
-// we'll require the Express.js npm package
-const express = require('express');
+// we'll require the following modules/packages
+const express = require('express'); // 3rd-party module
+const fs = require('fs'); // native to Node.js
+const path = require('path'); // native to Node.js
 // creating a route that the front-end
 // can request data from. Start by requiring the data
 const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
+
 // instantiate the server
 const app = express();
+// parse incoming string or array data. We used the app.use() method. This is a method executed
+// by our Express.js server that mounts a function to the server that our requests will pass through before 
+// getting to the intended endpoint. The functions we can mount to our server are referred to as middleware. 
+// The express.urlencoded({extended: true}) method is a method built into Express.js. It takes incoming POST
+// data and converts it to key/value pairings that can be accessed in the req.body object.
+// The extended: true option set inside the method call informs our server that there may be sub-array data
+// nested in it as well, so it needs to look as deep into the POST data as possible to parse all of the data correctly.
+// The express.json() method we used takes incoming POST data in the form of JSON and parses it into the
+// req.body JavaScript object. Both of the above middleware functions need to be set up every time you create
+// a server that's looking to accept POST data.
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 // create separate function filterByQuery to run code logic for
 // returning only the animals which meet the filtering requirements
@@ -56,6 +72,42 @@ function findById(id, animalsArray) {
   return result;
 }
 
+// used to add an animal object to the animals array
+// used in POST request method
+function createNewAnimal(body, animalsArray) {
+  // here you set the entirety of the body content to the constant animal
+  const animal = body;
+  // now you push that object to the end of the animal array
+  animalsArray.push(animal);
+  // we need to save the Javascript array data as JSON,
+  // so we use JSON.stringify() to convert it
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+
+  return animal;
+}
+
+// validate the existence and the type of data submitted during POST request
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== 'string') {
+    return false;
+  }
+  if (!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return true;
+}
+
+
+
 // adding the route the front-end can request data from
 // the get method takes a string which describes the route the
 // client will fetch from. the second parameter is a callback which
@@ -78,6 +130,8 @@ app.get('/api/animals/:id', (req, res) => {
   // to be defined in the route path, as shown above, i.e.
   // /api/animals/:id. The req.params filters by a single property, often
   // intended to retrieve a single record, i.e. the id of a particular animal
+  // This is in contrast to req.query, which is multifaceted, combining multiple
+  // parameters 
   const result = findById(req.params.id, animals);
   if(result) {
     res.json(result);
@@ -85,6 +139,23 @@ app.get('/api/animals/:id', (req, res) => {
     res.sendStatus(404);
   }
 })
+
+// POST request made from client side to server to accept data, rather
+// than retrieving data as in a GET request
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
+
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('The animal is not properly formatted.');
+  } else {
+  // add animal to json file and animals array in this function
+  const animal = createNewAnimal(req.body, animals);
+  res.json(animal);
+  }
+});
+
 
 // make our server listen by chaining the .listen method
 app.listen(PORT, () => {
